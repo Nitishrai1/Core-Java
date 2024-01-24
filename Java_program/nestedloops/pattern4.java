6def38eece44e419d2a44959480ef753e664bac1
Nitish RAI <115301219+Nitishrai1@users.noreply.github.com>
2024-01-24 15:23:14 +0000

    pattern

---
+++ Java_program/nestedloops/pattern4.java
@@ -0,0 +1,13 @@
+// Pattern4
+//     1
+//    21
+//   321
+//  4321
+public class pattern4{
+    public static void main(String[]args){
+        int num=4;
+        for(int i=1;i<=num;i++){
+            for(int)
+        }
+
+}
