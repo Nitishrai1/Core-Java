086dcf24e516cba7de6e82d5b55f3ae9d8d3835e
Nitish RAI <115301219+Nitishrai1@users.noreply.github.com>
2024-02-14 15:16:52 +0000

    string' .. clear f

---
+++ Strings/code1.java
@@ -0,0 +1,19 @@
+// package Strings;
+// When ever we create a string  it is created into heap because the string is a object bbut its varaibale name would be stored in the stack
+    // in java their in no function which can be used to manipulate the string character
+
+public class code1 {
+    
+    public static void main(String[] args){
+        
+        String s1="absdkjfk";
+        System.out.println(s1.length());
+            
+    
+        1
+    
+    }
+
+
+    
+}
---
+++ arrays/2Darray/code6.java
@@ -0,0 +1,4 @@
+public class code6{
+    // Multiplication of matrix
+    
+}
