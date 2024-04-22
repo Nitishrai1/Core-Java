d0105389853775d71d051591078452c1ee2509c7
Nitish RAI <115301219+Nitishrai1@users.noreply.github.com>
2024-04-22 17:34:45 +0000

    ioexception

--- Exception-handling/code1.java
+++ Exception-handling/code1.java
@@ -24,7 +24,15 @@ public class code1 {
 
         // }
         int arr[]=new int[4];
+        int l,j,k;
         try{
+            l=1;
+            j=0;
+            k=l/j;
+
+
+
+
             int m=sc.nextInt();
             for(int i=0;i<=4;i++){
                 arr[i]=sc.nextInt();
@@ -33,9 +41,20 @@ public class code1 {
             for(int i=0;i<m;i++){
                 System.out.println(arr[i]);
             }
-        }catch(Exception e){
-            System.out.println(e);
+            System.out.println(k);
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Number cannot be divided by zero "+e);  //this two are the known exception
+        }
+        catch(ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("Index out of range"+e);
 
+        }
+        catch(Exception e)
+        {
+            System.out.println("Unknown Exception"+e);  //this is for any other exception
         }
         
         
---
+++ Exception-handling/code2.java
@@ -0,0 +1,26 @@
+// IOException
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.*;
+
+public class code2 {
+    public static void main(String args[]){
+        int i,j,k;
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        try{
+            i=4;
+            j=Integer.parseInt(br.readLine());
+            System.out.println(i/j);
+
+
+        }catch(NumberFormatException e){
+            System.out.println("there is input error"+e);
+        }catch(IOException e){
+            System.out.println("error: "+e);
+        }catch(Exception e){
+            System.out.println("Exception "+e);
+        }
+
+    }
+    
+}
